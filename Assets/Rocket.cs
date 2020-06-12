a1e12b468a2cd29366e7c0c30b52b4f40d3b425c
razash10 <razash10@gmail.com>
2020-06-12 17:32:23 +0300

    Added movement to Beresheet

---
+++ Assets/Rocket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    void Update () {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            print("Thrusting");
+        }
+
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+
+        if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+
+    }
+}
